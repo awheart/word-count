9e1026fce076bc775602cd29ae1d45f383f0ae98
Awheart <arthurvandelanoitte@live.fr>
2022-10-27 14:28:14 +0200

    fix typo inside welcome message

--- src/main/java/cicd/learn/tp1/App.java
+++ src/main/java/cicd/learn/tp1/App.java
@@ -6,6 +6,6 @@ package cicd.learn.tp1;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("CICD rocks!");
+        System.out.println("CI/CD rocks!");
     }
 }
