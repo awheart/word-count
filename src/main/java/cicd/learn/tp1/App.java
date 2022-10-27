3e4bc28872c292e42b446ad34e083c57bd5319a5
Awheart <arthurvandelanoitte@live.fr>
2022-10-27 14:27:23 +0200

    update welcome message

--- src/main/java/cicd/learn/tp1/App.java
+++ src/main/java/cicd/learn/tp1/App.java
@@ -4,10 +4,8 @@ package cicd.learn.tp1;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("CICD rocks!");
     }
 }
