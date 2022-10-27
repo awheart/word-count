9e953608bca5232dc1cb96067d7c12757ba47784
Awheart <arthurvandelanoitte@live.fr>
2022-10-27 15:34:42 +0200

    update new welcome message

--- src/main/java/cicd/learn/tp1/App.java
+++ src/main/java/cicd/learn/tp1/App.java
@@ -7,5 +7,6 @@ package cicd.learn.tp1;
 public class App {
     public static void main(String[] args) {
         System.out.println("CI/CD rocks!");
+        System.out.println("CI/CD rocks feature 3!");
     }
 }
