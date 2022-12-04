5ad6d1b9a277c2dba4b5867dffccca5112cd6dda
Awheart <arthurvandelanoitte@live.fr>
2022-12-04 12:22:34 +0100

    new message

--- src/main/java/cicd/learn/tp1/App.java
+++ src/main/java/cicd/learn/tp1/App.java
@@ -9,5 +9,6 @@ public class App {
         System.out.println("CI/CD rocks fixed!");
         System.out.println("CI/CD rocks feature 2 commit final!");
         System.out.println("CI/CD rocks feature 3 commit final!");
+        System.out.println("CI/CD rocks feature 4!");
     }
 }
